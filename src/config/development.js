038a4d9da10df81d0b041fc542230a877a01aa81
Burra Kranthi Kiran <developer.startoonlabs@gmail.com>
2025-07-17 12:04:11 +0530

    base project

---
+++ src/config/development.js
@@ -0,0 +1,18 @@
+export const development_config = {
+  db: {
+    host: 'localhost',
+    database: 'events_app',
+    port: '3306',
+    username: 'root',
+    password: 'Kranthi@77',
+    seekAccount: false,
+    syncForce: false,
+    syncAlter: false,
+    seek: {
+      username: 'a',
+      password: '123',
+      companyName: 'QZT',
+    },
+    
+  },
+};
